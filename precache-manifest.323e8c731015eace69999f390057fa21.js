6f250680e0602f307b33a894e9f7281d546fe6cb
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-01-30 13:49:42 +0530

    Updates

---
+++ precache-manifest.323e8c731015eace69999f390057fa21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ade39e012ec2ab4314d7cef004606c",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "800aeeddac61e16d8eb5",
+    "url": "/tic-tac-toe/static/css/main.f51a99ae.chunk.css"
+  },
+  {
+    "revision": "b7a254b2292579644437",
+    "url": "/tic-tac-toe/static/js/2.01295b01.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.01295b01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "800aeeddac61e16d8eb5",
+    "url": "/tic-tac-toe/static/js/main.7830fe0e.chunk.js"
+  },
+  {
+    "revision": "fe8edb1d780f2938eb8e",
+    "url": "/tic-tac-toe/static/js/runtime-main.e7603406.js"
+  }
+]);
